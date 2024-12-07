85a88c146f7ee31e643e16e16e4269e9d5a1f518
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-12-07 18:00:29 +0100

    add draft route ranking function.

--- src/Router.ts
+++ src/Router.ts
@@ -1,36 +1,119 @@
-
+interface Route {
+    segments: string[],
+    callback: () => void
+}
 
 export default class Router {
 
-    #listening = false
+    #routes: Route[] = []
 
-    constructor() {
-        console.log('Router');
+    constructor() {}
+
+    // API ====================================================================
+
+    /**
+     * Adds a new route handler.
+     * @param {string} route - String route to match.
+     * @param {() => void} callback - Route handler.
+     * ```ts
+     * // Example
+     * router.add('/user/:id',            req => console.log(req.params.id))
+     * router.add('/user/*',              req => console.log(req.wildcards[0]))
+     * router.add('/user/settings/:page', req => console.log(req.params.page))
+     * ```
+     */
+    add(route: string, callback: () => void) {
+        this.#routes.push({
+            segments: route.split('/'),
+            callback
+        })
     }
 
-    mount() {
-        const hash = window.location.hash
-        if (!hash.startsWith('#/')) window.location.hash = hash.replace('#', '#/')
-        setTimeout(() => this.listen(), 0)
+    remove(route: string) {
+        this.#routes = this.#routes.filter(r => r.segments.join('/') !== route)
     }
 
-    listen() {
-        if (!this.#listening) {
-            this.#listening = true
-            window.addEventListener('hashchange', e => this.#handleHashChange(e))
+    // Ranking ================================================================
+
+    #EXACT_MATCH     = 3 as const
+    #PARAMETER_MATCH = 2 as const
+    #WILDCARD_MATCH  = 1 as const
+
+    /**
+     * Assigns a score to the the user-provided path based on its similarity to a resource route.
+     * The score is calculated as follows:
+     *  - Exact match: 3 points
+     *  - Parameter match: 2 points
+     *  - Wildcard match: 1 point
+     *  - Non-match: 0 points, scoring stops
+     * 
+     * Note that both parameters must be prepared by removing any leading or trailing slashes.
+     * Failure to do so will in a miscalculated score.
+     * @param {string[]} route - Resource route segments.
+     * @param {string[]} user - User route segments.
+     * @returns {number} Score.
+     */
+    rank(route: string[], user: string[]): number { 
+
+        let score = 0
+
+        for (let i = 0; i < route.length; i++) {
+
+            const uc = user[i]
+            const rc = route[i]
+
+            // Exact match
+            if (rc === uc) {
+                score += this.#EXACT_MATCH
+                continue
+            }
+
+            // Parameter match
+            if (rc && rc.startsWith(':')) {
+                score += this.#PARAMETER_MATCH
+                continue
+            }
+
+            // Trailing wildcard match
+            if (i === route.length - 1 && rc && rc === '*') {
+                if (!uc) return 0
+                const trailLength = user.length - i
+                score += trailLength * this.#WILDCARD_MATCH
+                break
+            }
+
+            // Wildcard match 
+            if (rc && rc === '*') {
+                score += this.#WILDCARD_MATCH
+                continue
+            }
+
+            return 0
+
         }
+        
+        return score
+
     }
 
-    off() {
-        if (this.#listening) {
-            this.#listening = false
-            window.removeEventListener('hashchange', e => this.#handleHashChange(e))
-        }
-    }
-
-    #handleHashChange(e: HashChangeEvent) {
-        const [route, queryString] = window.location.hash.replace(/#\/|#/, '').split('?')
-        const queries = queryString ? Object.fromEntries(new URLSearchParams(queryString)) : {}
-        console.log(route, queries)
-    } 
 }
+
+const r = new Router()
+const routes = [
+    'user/:id',
+    'user/*',
+    'user/*/test/*',
+    'user/settings/:page',
+    'user/*/hello',
+    '*/*/hello',
+    '*//hello',
+    'user/102954782/hello/*',
+]
+
+console.log(routes.map(route => [
+    r.rank(
+        route.split('/'), 
+        'user/102954782/hello'.split('/')
+    ), 
+    route
+]))
